afa40b9241c3ba588df776050e41e2c15ad59b36
ericpham <phamwangthang@gmail.com>
2023-03-07 23:47:53 +0100

    player input func

--- TicTacToe.py
+++ TicTacToe.py
@@ -5,3 +5,16 @@ def display_board(board):
 	print(board[4] + ' | ' + board[5]+ ' | ' + board[6])
 	print('---------')
 	print(board[1] + ' | ' + board[2]+ ' | ' + board[3])
+
+def player_input():
+	marker = ''
+	while marker != 'X' and marker != 'O':
+		marker = input('Player1: Choose X or O: ').upper()
+	if marker == 'X':
+		return ('X','O')
+	else:
+		return ('O','X')
+	
+def place_marker(board, marker, position):
+	board[position] = marker
+
